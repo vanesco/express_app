019a47b32099217f69cf95c5f41efda7d0e6b0b0
vanesco1005 <vanesco1005@gmail.com>
2020-07-12 01:58:58 +0900

    2020년 작업추가

---
+++ public/precache-manifest.0281baf8709e1a633df4a00967a64a09.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a201ec4b993809ec10",
+    "url": "/css/app.544ddce1.css"
+  },
+  {
+    "revision": "0fa41f7641c3b14b7323",
+    "url": "/css/chunk-11b1a7dc.bf4730e2.css"
+  },
+  {
+    "revision": "072e4959126f9d7f3f4e",
+    "url": "/css/chunk-1f4763ca.1ef48a67.css"
+  },
+  {
+    "revision": "668b87a4b6bc8bca7ed0",
+    "url": "/css/chunk-1f9ea9a1.7a5c8134.css"
+  },
+  {
+    "revision": "ba01ae5186a0bb728adc",
+    "url": "/css/chunk-2f843b33.c41fe788.css"
+  },
+  {
+    "revision": "2381c9a6aed2a95f407c",
+    "url": "/css/chunk-36ddbb84.c69f729e.css"
+  },
+  {
+    "revision": "097319f98323ef6bdc17",
+    "url": "/css/chunk-vendors.b872e5a8.css"
+  },
+  {
+    "revision": "725612757963add7a3fcf985f2d12eb2",
+    "url": "/img/advantage_img.72561275.png"
+  },
+  {
+    "revision": "beb217e1eb8526f4453628c20ce4b6d8",
+    "url": "/img/bridge_icon.beb217e1.png"
+  },
+  {
+    "revision": "51cd2f12dd7c99c7a4a43620e1c8c8ae",
+    "url": "/img/bridge_img.51cd2f12.jpg"
+  },
+  {
+    "revision": "a11efc27ffbd08c8f4d99a0dd27f9c38",
+    "url": "/img/careers-background.a11efc27.jpg"
+  },
+  {
+    "revision": "7b0ac77cf486cdbe4a571cdf953ec4cc",
+    "url": "/img/careers_2013.7b0ac77c.png"
+  },
+  {
+    "revision": "b66d5c4ee3a91411bbee6707b2cd05f8",
+    "url": "/img/careers_2014.b66d5c4e.png"
+  },
+  {
+    "revision": "7e07dcc8864318e59cee177311486b3c",
+    "url": "/img/careers_2015.7e07dcc8.png"
+  },
+  {
+    "revision": "e885b58c49655877b15570b289aad1b6",
+    "url": "/img/careers_2016.e885b58c.png"
+  },
+  {
+    "revision": "2b1e7877c1fea3ca2e2bfb0bffa282c2",
+    "url": "/img/careers_2017.2b1e7877.png"
+  },
+  {
+    "revision": "9b3752f4221fb2b1f62b59c917078e49",
+    "url": "/img/careers_2018.9b3752f4.png"
+  },
+  {
+    "revision": "c41708db7f5212a1b46a3a6b0d8c3412",
+    "url": "/img/careers_2019.c41708db.png"
+  },
+  {
+    "revision": "4d1a42e86ceddaa929d93a2b84debfb0",
+    "url": "/img/careers_2020.4d1a42e8.png"
+  },
+  {
+    "revision": "11105488d6637d67aaca91b402adea9b",
+    "url": "/img/celebme.11105488.jpg"
+  },
+  {
+    "revision": "ec0a97ac3cf76c21f6f7fbfb9fa4aa9c",
+    "url": "/img/celebme_m.ec0a97ac.jpg"
+  },
+  {
+    "revision": "539807e52a566e376f10b9ec4d656d7b",
+    "url": "/img/contact_icon.539807e5.png"
+  },
+  {
+    "revision": "ae2f59a154c24490fa49c81a0dfb2fa7",
+    "url": "/img/css3_logo.ae2f59a1.jpg"
+  },
+  {
+    "revision": "ad9bf0936a123acee6077c2f3ab5527e",
+    "url": "/img/esMall.ad9bf093.jpg"
+  },
+  {
+    "revision": "472d63c7bf24927632efaa97c0287f29",
+    "url": "/img/github-logo.472d63c7.png"
+  },
+  {
+    "revision": "61fb086256b8ec12580bf5ce741472a5",
+    "url": "/img/html5_logo.61fb0862.jpg"
+  },
+  {
+    "revision": "fc1c63521b17feb7a71e815eefb38567",
+    "url": "/img/introduce_img.fc1c6352.png"
+  },
+  {
+    "revision": "31cd4c7ef5d9c276aa8329c1f73bfbb2",
+    "url": "/img/js_logo.31cd4c7e.jpg"
+  },
+  {
+    "revision": "4e5a8e5ff497a8783843bd3ad4ba4782",
+    "url": "/img/name-biz.4e5a8e5f.jpg"
+  },
+  {
+    "revision": "0847a399bdb99c3d71c7c773d94db865",
+    "url": "/img/particle-background.0847a399.jpg"
+  },
+  {
+    "revision": "c705fb3fbbcde4e51d377e5df9dcd3f5",
+    "url": "/img/product_01.c705fb3f.jpg"
+  },
+  {
+    "revision": "2ea4ee8bb5f745f46a9fce6a27dcc125",
+    "url": "/img/product_02.2ea4ee8b.jpg"
+  },
+  {
+    "revision": "67075b89febb7e46f0dfc8297898632e",
+    "url": "/img/product_03.67075b89.jpg"
+  },
+  {
+    "revision": "0b7d0fce32ba9e87e41e524288765aef",
+    "url": "/img/product_04.0b7d0fce.jpg"
+  },
+  {
+    "revision": "c5c2b21a7d4fa0f30e07547937574165",
+    "url": "/img/product_05.c5c2b21a.jpg"
+  },
+  {
+    "revision": "94eb34c8edad2d8a10d16471226fac44",
+    "url": "/img/product_06.94eb34c8.jpg"
+  },
+  {
+    "revision": "002d9102a24f741881a4494525200c19",
+    "url": "/img/soso.002d9102.jpg"
+  },
+  {
+    "revision": "42e6936811c464871b21dd22bd936206",
+    "url": "/img/soso_m.42e69368.jpg"
+  },
+  {
+    "revision": "01b65d506e49f5847707d4914c26d78b",
+    "url": "/img/wakan.01b65d50.jpg"
+  },
+  {
+    "revision": "dd5c064d2f23325cd3b1c3f39facadb0",
+    "url": "/img/work_01.dd5c064d.jpg"
+  },
+  {
+    "revision": "bb01827d243cbbe6462a855390a1055d",
+    "url": "/img/work_02.bb01827d.jpg"
+  },
+  {
+    "revision": "33fbed0e3ff1f40cb595bb3ecd0d278e",
+    "url": "/img/work_03.33fbed0e.jpg"
+  },
+  {
+    "revision": "55cfdbce92d003c15377e39e61477fd6",
+    "url": "/img/work_04.55cfdbce.jpg"
+  },
+  {
+    "revision": "a2bf8c40c027ebfd73e0792c7dc3c3c8",
+    "url": "/img/work_05.a2bf8c40.jpg"
+  },
+  {
+    "revision": "887945b54c6110501220455471986e30",
+    "url": "/img/work_06.887945b5.jpg"
+  },
+  {
+    "revision": "2ecf950702055eadfecd25b7ace765f6",
+    "url": "/img/work_07.2ecf9507.jpg"
+  },
+  {
+    "revision": "d63704ca096cbe5be3a553b06dc8ee49",
+    "url": "/img/work_08.d63704ca.jpg"
+  },
+  {
+    "revision": "678a0a527a04e103fa285308494c2bd5",
+    "url": "/img/work_09.678a0a52.jpg"
+  },
+  {
+    "revision": "b3feb7e8e52eff9c6f2170b922580ca0",
+    "url": "/img/work_10.b3feb7e8.jpg"
+  },
+  {
+    "revision": "3ecabab57ab184193965ea198a5e78f2",
+    "url": "/img/work_11.3ecabab5.jpg"
+  },
+  {
+    "revision": "18628b8304cc4e3513cf9edd7909de4b",
+    "url": "/img/work_12.18628b83.jpg"
+  },
+  {
+    "revision": "c66cea9fc19b7ff41c678771bc6cfe62",
+    "url": "/img/work_13.c66cea9f.jpg"
+  },
+  {
+    "revision": "830a9348f9aeadbd6f96f498340c4316",
+    "url": "/img/work_14.830a9348.jpg"
+  },
+  {
+    "revision": "40db15ae1625b7591915d6c4dc6e4261",
+    "url": "/img/work_15.40db15ae.jpg"
+  },
+  {
+    "revision": "3a32867abb738292229b9bd784c24361",
+    "url": "/img/work_icon.3a32867a.png"
+  },
+  {
+    "revision": "11a3a4e0e64e1ae69b09e5e9cdb20a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a201ec4b993809ec10",
+    "url": "/js/app.676ccd31.js"
+  },
+  {
+    "revision": "0fa41f7641c3b14b7323",
+    "url": "/js/chunk-11b1a7dc.382c1faa.js"
+  },
+  {
+    "revision": "072e4959126f9d7f3f4e",
+    "url": "/js/chunk-1f4763ca.99310401.js"
+  },
+  {
+    "revision": "668b87a4b6bc8bca7ed0",
+    "url": "/js/chunk-1f9ea9a1.bcde27be.js"
+  },
+  {
+    "revision": "ba01ae5186a0bb728adc",
+    "url": "/js/chunk-2f843b33.9306dba9.js"
+  },
+  {
+    "revision": "2381c9a6aed2a95f407c",
+    "url": "/js/chunk-36ddbb84.eab216d4.js"
+  },
+  {
+    "revision": "097319f98323ef6bdc17",
+    "url": "/js/chunk-vendors.219af2ec.js"
+  },
+  {
+    "revision": "9b7689a41debcd47684c84cde27b3c06",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.428d39b009fc21db5797736c136c3c7f.js"
+  "/precache-manifest.0281baf8709e1a633df4a00967a64a09.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "vue-app"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
