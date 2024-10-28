e36914cc7d306cfa3cc0e461ee74d8d86b45a3d4
vanesco <vanesco@forbiz.co.kr>
2024-10-28 17:09:52 +0900

    2024 재정비

--- app.js
+++ app.js
@@ -31,9 +31,5 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
 module.exports = app;
 // test
